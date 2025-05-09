1fe1fcc7280e41930c996a9811145c0fa4a82021
TaunPro <yentuan130803@gmail.com>
2025-05-10 00:10:06 +0700

    test CI/CD for product-service

--- product-service/src/main/java/vn/tt/practice/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/vn/tt/practice/productservice/ProductServiceApplication.java
@@ -6,13 +6,21 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
+@RestController
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
