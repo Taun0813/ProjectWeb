3427ee4bf40333b335b33e9741fe45c93042ea7e
TaunPro <yentuan130803@gmail.com>
2025-05-12 19:20:38 +0700

    test cI/CD pt 16

--- product-service/src/main/java/vn/tt/practice/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/vn/tt/practice/productservice/ProductServiceApplication.java
@@ -21,6 +21,6 @@ public class ProductServiceApplication {
 
     @GetMapping("/test")
     public String test() {
-        return "Test CI/CD pt15 !!!";
+        return "Test CI/CD pt16 !!!";
     }
 }
